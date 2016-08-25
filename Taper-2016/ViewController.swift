40b5e05e79ad2ea532d1350748c1a03572133f80
Briano Santos <brianosantos@gmail.com>
2016-08-25 16:28:44 -0700

    1) created the layout and applied constraints, 2) created 5 outlets

--- Taper-2016/ViewController.swift
+++ Taper-2016/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    
+    @ IBOutlet weak var logoImgView: UIImageView!
+    @ IBOutlet weak var howManyTapsText: UITextField!
+    @ IBOutlet weak var playBtn: UIButton!
+    
+    @ IBOutlet weak var tapBtn: UIButton!
+    @ IBOutlet weak var tapsLbl: UILabel!
+    
+    
 
 }
 
