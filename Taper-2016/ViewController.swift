582324cd51ba6576236a4e8b753f95ceb42059a8
Briano Santos <brianosantos@gmail.com>
2016-08-29 22:21:54 -0700

    1) fixed a bug: app would crash when PlayButton was pressed and there was an empty string in the UITextField. 2) Changed the name of an outlet to make it easier to understand how it connected to the storyboard."

--- Taper-2016/ViewController.swift
+++ Taper-2016/ViewController.swift
@@ -11,14 +11,43 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
     
+    
+    //Outlets
     @ IBOutlet weak var logoImg: UIImageView!
-    @ IBOutlet weak var howManyTapsText: UITextField!
+    @ IBOutlet weak var howManyTapsTxt: UITextField!
     @ IBOutlet weak var playBtn: UIButton!
     
     @ IBOutlet weak var tapBtn: UIButton!
     @ IBOutlet weak var tapsLbl: UILabel!
     
+    @ IBAction func onPlayButtonPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            tapsLbl.text = "\(currentTaps) Taps"
+            
+        }
+    }
+    
+    @ IBAction func onCoinBtnPressed(sender: UIButton!) {
+        
+        
+        
+    }
     
 
 }
